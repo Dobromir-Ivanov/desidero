75c2d18ef4e1d830fb9f4e0a6311bc75c4529000
Dobromir Ivanov <dobromirivanov@hotmail.com>
2020-12-06 20:16:31 +0100

    Remove frontend default template .

--- desidero/desidero/ClientApp/src/app/components/home/home.component.ts
+++ desidero/desidero/ClientApp/src/app/components/home/home.component.ts
@@ -4,17 +4,14 @@
 // =============================
 
 import { Component } from '@angular/core';
-import { fadeInOut } from '../../services/animations';
-import { ConfigurationService } from '../../services/configuration.service';
 
 
 @Component({
-    selector: 'home',
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.scss'],
-    animations: [fadeInOut]
+  selector: 'home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
-    constructor(public configurations: ConfigurationService) {
-    }
+  constructor() {
+  }
 }
